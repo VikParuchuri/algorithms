d2ae5a470b3281ae656f81da49cd1bbccc62b5e3
Vik Paruchuri <vik.paruchuri@gmail.com>
2013-07-14 09:39:33 -0400

    Matrix inversion algorithm

---
+++ base.py
@@ -0,0 +1,132 @@
+from __future__ import division
+from copy import deepcopy
+
+
+class Algorithm(object):
+    def __init__(self):
+        pass
+
+    def train(self, X, y):
+        pass
+
+    def predict(self, X, y):
+        pass
+
+class Matrix(object):
+    def __init__(self, X):
+        self.validate(X)
+        self.X = X
+
+    def validate(self, X):
+        list_error = "X must be a list of lists corresponding to a matrix, with each sub-list being a row."
+        if not isinstance(X, list):
+            raise Exception(list_error)
+        if not isinstance(X[0], list):
+            raise Exception(list_error)
+        first_row_len = len(X[0])
+        for i in xrange(0,len(X)):
+            if len(X[i])!=first_row_len:
+                raise Exception("All rows in X must be the same length.")
+
+def check_for_all_zeros(X,i,j):
+    non_zeros = []
+    first_non_zero = -1
+    for m in xrange(i,len(X)):
+        non_zero = X[m][j]!=0
+        non_zeros.append(non_zero)
+        if first_non_zero==-1 and non_zero:
+            first_non_zero = m
+    zero_sum = sum(non_zeros)
+    return zero_sum, first_non_zero
+
+def swap_row(X,i,p):
+    X[p], X[i] = X[i], X[p]
+    return X
+
+def make_identity(r,c):
+    identity = []
+    for i in xrange(0,r):
+        row = []
+        for j in xrange(0,c):
+            elem = 0
+            if i==j:
+                elem = 1
+            row.append(elem)
+        identity.append(row)
+    return identity
+
+
+def invert(X):
+    """
+    Invert a matrix X according to gauss-jordan elimination
+    X - input list of lists where each list is a matrix row
+    output - inverse of X
+    """
+    #copy X to avoid altering input
+    X = deepcopy(X)
+
+    #Get dimensions of X
+    rows = len(X)
+    cols = len(X[0])
+
+    #Get the identity matrix and append it to the right of X
+    #This is done because our row operations will make the identity into the inverse
+    identity = make_identity(rows,cols)
+    for i in xrange(0,rows):
+        X[i]+=identity[i]
+
+    i = 0
+    for j in xrange(0,cols):
+        print("On col {0} and row {1}".format(j,i))
+        #Check to see if there are any nonzero values below the current row in the current column
+        zero_sum, first_non_zero = check_for_all_zeros(X,i,j)
+        #If everything is zero, increment the columns
+        if zero_sum==0:
+            if j==cols:
+                return X
+            continue
+        #If X[i][j] is 0, and there is a nonzero value below it, swap the two rows
+        if first_non_zero != i:
+            X = swap_row(X,i,first_non_zero)
+        X[i] = [m/X[i][j] for m in X[i]]
+        for q in xrange(0,rows):
+            if q!=i:
+                scaled_row = [X[q][j] * m for m in X[i]]
+                X[q]= [X[q][m] - scaled_row[m] for m in xrange(0,len(scaled_row))]
+        if i==rows or j==cols:
+            break
+        print X
+        i+=1
+
+    for i in xrange(0,rows):
+        X[i] = X[i][cols:len(X[i])]
+
+    return X
+
+def mean(l):
+    sum(l)/len(l)
+
+def gje(X):
+    X = deepcopy(X)
+    rows = len(X)
+    cols = len(X[0])
+
+    identity = make_identity(rows,cols)
+    for i in xrange(0,rows):
+        X[i]+=identity[i]
+
+    for k in xrange(0,rows):
+        abs_list = [abs(X[i][k]) for i in xrange(k,rows)]
+        i_max  = abs_list.index(max(abs_list))+k
+        if X[i_max][k] == 0:
+            raise Exception("Matrix is singular!")
+        X = swap_row(X, k, i_max)
+        for i in xrange(k+1, rows):
+            for j in xrange(k, cols):
+                X[i][j]  = X[i][j] - X[k][j] * (X[i][j] / X[j][j])
+            X[i][k]  = 0
+
+    for i in xrange(0,rows):
+        X[i] = X[i][int(cols):len(X[i])]
+
+    return X
---
+++ linreg.py
@@ -0,0 +1,19 @@
+from base import Algorithm, mean
+
+class LinregNonMatrix(Algorithm):
+    def train(self, x, y):
+        x_mean = mean(x)
+        y_mean = mean(y)
+        x_dev = sum([i-x_mean for i in x])
+        y_dev = sum([i-y_mean for i in y])
+
+        self.slope = (x_dev*y_dev)/(x_dev*x_dev)
+        self.intercept = y_mean - (self.slope*x_mean)
+
+    def predict(self, x):
+        return [i*self.slope + self.intercept for i in x]
+
+class LinregListMatrix(Algorithm):
+    def train(self, X, y):
+        pass
+
